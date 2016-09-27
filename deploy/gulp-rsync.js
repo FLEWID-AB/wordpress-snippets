1ea6e99770bf13a3cad99fbb8d1514c01e487b44
Philip Mannheimer <philip.mannheimer@gmail.com>
2016-09-27 15:53:58 +0200

    Update gulp-rsync.js
    Added environment (eg staging, production) and Prompt to confirm you're going to prod.

--- deploy/gulp-rsync.js
+++ deploy/gulp-rsync.js
@@ -1,7 +1,11 @@
+var argv   = require('minimist')(process.argv);
+var gulpif = require('gulp-if');
+var prompt = require('gulp-prompt');
 var rsync  = require('gulp-rsync');
 var chmod  = require('gulp-chmod');
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['build'], function() {
+
   var paths = [
     "*.php", 
     "*.css", 
@@ -24,14 +28,34 @@ gulp.task('deploy', function() {
     .pipe(chmod(644))
     .pipe(gulp.dest('dist'));
 
+
+  // Rsync options
+  var rsyncOptions = {
+    root: 'dist',
+    incremental: true,  
+    progress: true  
+  }
+
+  // Check for prod or staging
+  if (argv.production) {
+    rsyncOptions.hostname = 'HOSTNAME'; // Prod hostname
+    rsyncOptions.username = 'USER'; // Prod ssh username
+    rsyncOptions.destination = ''; // Prod path where uploaded files go
+  } else if(argv.staging){
+    rsyncOptions.hostname = 'HOSTNAME'; // Staging hostname
+    rsyncOptions.username = 'USER'; // SSH Staging username
+    rsyncOptions.destination = ''; // Staging path where uploaded files go
+  } else {
+    return false
+  }
+
   // Rsync to server
   return gulp.src('./dist/**/*')
-    .pipe(rsync({
-      root: 'dist',
-      incremental: true,
-      username: 'USER',
-      hostname: 'XXX',
-      progress: true,
-      destination: 'DIRECT PATH TO WP THEME DIR'
-    }));
+    .pipe(gulpif(
+      argv.production, 
+      prompt.confirm({
+        message: 'Heads Up! Are you SURE you want to push to PRODUCTION?',
+        default: false
+    })))  
+    .pipe(rsync(rsyncOptions));
 });
